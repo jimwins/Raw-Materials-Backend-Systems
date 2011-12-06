f169fb9a445076fa28a948fcda8cf8bfddf0e4ee
jim winstead <jimw@trainedmonkey.com>
2011-12-05 21:02:17 -0800

    Add a common file for shared code, move stuff there

---
+++ common.php
@@ -0,0 +1,13 @@
+<?php
+
+function generate_jsonp($data) {
+  if (preg_match('/\W/', $_GET['callback'])) {
+    // if $_GET['callback'] contains a non-word character,
+    // this could be an XSS attack.
+    header('HTTP/1.1 400 Bad Request');
+    exit();
+  }
+  header('Content-type: application/javascript; charset=utf-8');
+  print sprintf('%s(%s);', $_GET['callback'], json_encode($data));
+}
+
